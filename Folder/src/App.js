57133b49512fc50a200800ec3447e21aae86b4fa
Priyanshi051002 <202001441@daiict.ac.in>
2023-03-11 20:18:24 +0530

    initial commit

---
+++ Folder/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Signup from './components/signup';
+// import Login from './components/Login';
+function App() {
+  return (
+    <div className="App">
+      {/* <Login/> */}
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
